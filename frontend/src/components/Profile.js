3ba3beaa39b69a6dde513f6a5c92ea7b6db15d27
Vlad Vashchuk <50277357+Vladzo@users.noreply.github.com>
2023-01-18 14:45:45 +0000

    move follow button to the left

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -126,8 +126,8 @@ class Profile extends React.Component {
     return (
       <div className="profile-page">
         <div className="container">
-          <div className="row p-4 text-center">
-            <div className="user-info col-xs-12 col-md-8 offset-md-2">
+          <div className="row p-4">
+            <div className="user-info .col-">
               <img
                 src={profile.image}
                 className="user-img"
